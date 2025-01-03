2ffbf675bb950040939092e47e4a37caa31a1de0
Asa Tsai <karoteglathe@gmail.com>
2025-01-04 02:02:10 +0800

    update element row gap

--- apps/ui/src/contents/Element.tsx
+++ apps/ui/src/contents/Element.tsx
@@ -11,12 +11,12 @@ const Element = ({ elementId }: { elementId: string }) => {
   const { name, type, lock, visible } = elementInstance.value
   return (
     <div className="flex items-center justify-between p-2 hover:bg-panel-light cursor-pointer text-gray-200">
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-1 gap-1">
         <Icon name={type as EntityTypes} />
         {name}
       </div>
 
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-1">
         <Icon name={lock ? 'Lock' : 'Unlock'} />
         <Icon name={visible ? 'Visible' : 'Invisible'} />
       </div>
