b921bb7646785552eb8948ec28baf8cd2d9114ce
Asa Tsai <karoteglathe@gmail.com>
2025-02-08 17:34:41 +0800

    use 2 terminal for yarn react start and watch design system

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
+import dts from 'vite-plugin-dts'
 import tailwindcss from 'tailwindcss'
+import { resolve } from 'path'
 import peerDependencies from './package.json'
 
 export default defineConfig({
@@ -18,26 +20,31 @@ export default defineConfig({
       svgrOptions: {
         icon: true
       }
-    })
+    }),
+    dts()
   ],
   build: {
     lib: {
-      entry: './src/index.tsx',
+      entry: resolve(__dirname, './src/index.tsx'),
       name: 'DesignSystem',
-      fileName: (format) => `design-system.${format}.js`,
-      formats: ['es', 'umd']
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies), /\.css$/],
+      external: [...Object.keys(peerDependencies), 'tailwindcss'],
       output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name?.endsWith('.css')) {
-            return 'index.css'
-          }
-          return assetInfo.name || 'assets/[name]-[hash][extname]'
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss'
         }
       }
     },
+    sourcemap: true,
     emptyOutDir: false
+  },
+  server: {
+    watch: {
+      ignored: ['**/dist/**']
+    }
   }
 })
