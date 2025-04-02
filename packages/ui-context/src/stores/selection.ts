7b1326ee634814b3b07c7d45d5b894c246dcf8bf
Asa Tsai <karoteglathe@gmail.com>
2025-04-02 21:15:34 +0800

    move selection subject to ui-context root

--- packages/ui-context/src/stores/selection.ts
+++ packages/ui-context/src/stores/selection.ts
@@ -1,24 +1,13 @@
-import { BehaviorSubject } from 'rxjs'
 import { SelectionManager } from '@asra/selection'
 import { ComputedAttrs, SELECTION_TYPES } from '@asra/utils'
+import sceneTree from '@asra/scene-tree'
 import uiContext from '../ui-context'
-import sceneTree from '@asra/scene-tree/dist/sceneTree'
-
-const SelectionTypes = Object.values(SELECTION_TYPES)
-type SelectionDataTye = Set<string>
 
 export default class SelectionStore {
   selectionManager: SelectionManager
-  selections: Map<string, BehaviorSubject<SelectionDataTye>> = new Map()
 
   constructor(selectionManager: SelectionManager) {
     this.selectionManager = selectionManager
-    SelectionTypes.forEach((type: SELECTION_TYPES) => {
-      this.selections.set(
-        type,
-        new BehaviorSubject<SelectionDataTye>(new Set())
-      )
-    })
   }
 
   updateSelection(type: SELECTION_TYPES) {
@@ -27,9 +16,7 @@ export default class SelectionStore {
       return
     }
 
-    const selected = this.selections.get(type)
     const selectedIds = selection.getSelectedIds()
-    selected?.next(new Set(selectedIds))
 
     switch (type) {
       case SELECTION_TYPES.ELEMENT:
@@ -50,13 +37,5 @@ export default class SelectionStore {
         uiContext.updateVertexSelection(selectedIds)
         break
     }
-  }
-
-  get elements() {
-    return this.selections.get(SELECTION_TYPES.ELEMENT)
-  }
-
-  get vertex() {
-    return this.selections.get(SELECTION_TYPES.VERTEX)
   }
 }
