d1eefe8b64a03c0e60e638e540519c32e14bfffb
Asa Tsai <karoteglathe@gmail.com>
2025-04-15 18:20:12 +0800

    enable Text to accept number

--- packages/design-system/src/components/Text/Text.tsx
+++ packages/design-system/src/components/Text/Text.tsx
@@ -5,14 +5,15 @@ export interface TextProps {
   /** Whether the text is disabled */
   disabled?: boolean
   /** Content inside the button */
-  label: string
+  label: string | number
   /** Content inside the button */
   classNames?: string
 }
 
 // Text Component
 const Text: React.FC<TextProps> = ({ label, classNames = 'text-gray-200' }) => {
-  return <span className={classNames}>{label}</span>
+  const labelContent = typeof label === 'string' ? label : label.toString()
+  return <span className={classNames}>{labelContent}</span>
 }
 
 export default Text
