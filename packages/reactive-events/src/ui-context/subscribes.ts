956b48596a1de4d275f18c7b34ed6030ee736699
Asa Tsai <karoteglathe@gmail.com>
2025-05-13 21:44:50 +0800

    use createSubscribeEvent for ui-context events

--- packages/reactive-events/src/ui-context/subscribes.ts
+++ packages/reactive-events/src/ui-context/subscribes.ts
@@ -1,34 +1,17 @@
 import { Subscription } from 'rxjs'
-import { filter } from 'rxjs/operators'
 import type {
   FinishRequestElementSelectionEvent,
   RequestElementSelectionEvent
 } from './events'
-import { getEventBusObserve } from '../event-bus'
+import { createSubscribeEvent } from '../event-bus'
 import { EventTypes } from '../types'
 
-export const subscribeToRequestElementSelection = (
-  subscriber: (event: RequestElementSelectionEvent) => void
-): Subscription => {
-  return getEventBusObserve()
-    .pipe(
-      filter(
-        (event): event is RequestElementSelectionEvent =>
-          event.type === EventTypes.REQUEST_ELEMENT_SELECTION
-      )
-    )
-    .subscribe(subscriber)
-}
+export const subscribeToRequestElementSelection =
+  createSubscribeEvent<RequestElementSelectionEvent>(
+    EventTypes.REQUEST_ELEMENT_SELECTION
+  )
 
-export const subscribeToFinishRequestElementSelection = (
-  subscriber: (event: FinishRequestElementSelectionEvent) => void
-): Subscription => {
-  return getEventBusObserve()
-    .pipe(
-      filter(
-        (event): event is FinishRequestElementSelectionEvent =>
-          event.type === EventTypes.FINISH_REQUEST_ELEMENT_SELECTION
-      )
-    )
-    .subscribe(subscriber)
-}
+export const subscribeToFinishRequestElementSelection =
+  createSubscribeEvent<FinishRequestElementSelectionEvent>(
+    EventTypes.FINISH_REQUEST_ELEMENT_SELECTION
+  )
