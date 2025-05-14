353575d510c235fcc8b90669280e8f54fad1a999
Asa Tsai <karoteglathe@gmail.com>
2025-05-14 21:20:26 +0800

    fix lint

--- apps/ui/src/properties/rotation.tsx
+++ apps/ui/src/properties/rotation.tsx
@@ -6,7 +6,13 @@ const Rotation = () => {
   return (
     <div className="flex items-center gap-2 text-gray-200 w-full px-3 py-1">
       <div className="w-1/2">
-        <Input value={rotation} prefix="R" onChange={() => {}} />
+        <Input
+          value={rotation}
+          prefix="R"
+          onChange={() => {
+            // TODO:
+          }}
+        />
       </div>
     </div>
   )
