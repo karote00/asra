988c21a194e2a3d6c13d7d447081f6515b3a0278
Asa Tsai <karoteglathe@gmail.com>
2025-05-20 21:10:13 +0800

    fix: use API interface

--- packages/system-context/src/system-context.ts
+++ packages/system-context/src/system-context.ts
@@ -2,13 +2,15 @@ import { MouseSnapshot } from '@asra/utils'
 import { createAllAPIs } from './apis'
 import { primaryToolState, mouseState } from './states'
 import { initSystemContextSubscribe } from './subscribe'
-import { HandlerDeps, SystemContextAPIs } from './types'
+import { HandlerDeps, MouseStateAPIs, SystemContextAPIs } from './types'
 import { PrimaryToolAPIs } from './types/primary-tool'
 
 export class SystemContext implements SystemContextAPIs {
   getCurrentPrimaryTool!: PrimaryToolAPIs['getCurrentPrimaryTool']
   switchPrimaryTool!: PrimaryToolAPIs['switchPrimaryTool']
-  updateMouseState!: (mouseSnapshot: MouseSnapshot) => void
+
+  updateMouseState!: MouseStateAPIs['updateMouseState']
+  getMouseState!: MouseStateAPIs['getMouseState']
 
   constructor(deps: HandlerDeps) {
     const apis = createAllAPIs(deps)
