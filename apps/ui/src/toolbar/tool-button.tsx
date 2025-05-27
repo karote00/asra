e10e41344bd89b5bc751ce7087cb2716f5ebfcfd
Asa Tsai <karoteglathe@gmail.com>
2025-05-27 16:34:43 +0800

    feature: add select tool icon on top-tools (#30)

--- apps/ui/src/toolbar/tool-button.tsx
+++ apps/ui/src/toolbar/tool-button.tsx
@@ -2,18 +2,26 @@ import { Icon } from '@asra/design-system'
 import { usePrimaryTool } from '../providers'
 import { PrimaryToolType } from '@asra/utils'
 
+const selectedStyle =
+  'bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-white'
+const normalStyle =
+  'hover:bg-gray-100 text-gray-500 dark:hover:bg-gray-800 dark:text-gray-400'
+
 const ToolButton = () => {
   const primaryTool = usePrimaryTool()
+  const selectToolStyle =
+    primaryTool === PrimaryToolType.SELECT ? selectedStyle : normalStyle
+  const rectangleToolStyle =
+    primaryTool === PrimaryToolType.RECTANGLE ? selectedStyle : normalStyle
 
   return (
-    <div
-      className={`flex text-white align-middle ${
-        primaryTool === PrimaryToolType.RECTANGLE
-          ? 'bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-white'
-          : 'hover:bg-gray-100 text-gray-500 dark:hover:bg-gray-800 dark:text-gray-400'
-      }`}
-    >
-      <Icon name="Rectangle" />
+    <div className="flex text-white">
+      <div className={`flex align-middle ${selectToolStyle}`}>
+        <Icon name="Select" />
+      </div>
+      <div className={`flex align-middle ${rectangleToolStyle}`}>
+        <Icon name="Rectangle" />
+      </div>
     </div>
   )
 }
