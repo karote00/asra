7b9c0177662468d184ca37cb299fe331222a7714
Asa Tsai <karoteglathe@gmail.com>
2026-02-07 21:18:41 +0800

    cleanup: remove obsolete rule files and disable old subscribers
    - Deleted create-element-rules.ts (replaced by feature-system)
    - Deleted resize-element-rules.ts (replaced by feature-system)
    - Deleted reset-element-size-rules.ts (replaced by feature-system)
    - Cleaned up rules/index.ts exports
    - Disabled select-elements.ts subscriber (now handled by feature-system)
    - Cleaned up commented code in create-element.ts subscriber
    
    Note: selection workflow is 80% refactored, keeping old behavior/interaction
    files for now as they're still partially in use.
    
    Files changed:
    - apps/asyra-design/src/init/rules/create-element-rules.ts (deleted)
    - apps/asyra-design/src/init/rules/resize-element-rules.ts (deleted)
    - apps/asyra-design/src/init/rules/reset-element-size-rules.ts (deleted)
    - apps/asyra-design/src/init/rules/index.ts
    - apps/asyra-design/src/init/subscribers/select-elements.ts

--- apps/asyra-design/src/init/rules/index.ts
+++ apps/asyra-design/src/init/rules/index.ts
@@ -1,11 +1,5 @@
 export * from './move-rules'
 export * from './select-rules'
-// Note: create-element-rules replaced by feature-system
-// export * from './create-element-rules'
-// Note: resize-element-rules replaced by feature-system
-// export * from './resize-element-rules'
-// Note: reset-element-size-rules replaced by feature-system
-// export * from './reset-element-size-rules'
 export * from './undoredo-rules'
 export * from './zoomfit-rules'
 export * from './panzoom-rules'
--- apps/asyra-design/src/init/subscribers/select-elements.ts
+++ apps/asyra-design/src/init/subscribers/select-elements.ts
@@ -1,13 +1,17 @@
 /**
  * Subscribers - Simple forwarding layer
+ *
+ * DISABLED: Now using feature-system for selection
+ * Old subscribers disabled to avoid duplicate selection handling
  */
 
-import { subscribeToDecideToSelectElements } from '../events'
-import { selectionApis } from '../apis'
+// import { subscribeToDecideToSelectElements } from '../events'
+// import { selectionApis } from '../apis'
 
 export const initSelectElementsSubscribers = () => {
-  subscribeToDecideToSelectElements((payload) => {
-    const { elementIds } = payload as any
-    selectionApis.selectElements(elementIds)
-  })
+  // Disabled - moved to feature-system selection feature
+  // subscribeToDecideToSelectElements((payload) => {
+  //   const { elementIds } = payload as any
+  //   selectionApis.selectElements(elementIds)
+  // })
 }
