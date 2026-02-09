b28dade4624aa33514f72abc8af0469ac1b7835d
Asa Tsai <karoteglathe@gmail.com>
2026-02-10 00:19:33 +0800

    debug: remove console logging from property changes
    Removed debug console logs from:
    - controllers/scene-tree.ts - removed [changeElementComputedData] logging
    - packages/utils/src/setter.ts - removed [Setter.set] logging
    
    Property changes are working correctly, debug logs no longer needed.

--- apps/asyra-design/src/controllers/scene-tree.ts
+++ apps/asyra-design/src/controllers/scene-tree.ts
@@ -2,15 +2,7 @@ import { DataTypes } from '@asyra/utils'
 import { elementApis, selectionApis, transactionApis } from '../common-apis'
 
 export const changeElementComputedData = (key: string, data: DataTypes) => {
-  const selectedIds = selectionApis.getSelectedIds()
-  console.log('[changeElementComputedData]', { key, data, selectedIds })
-
-  if (selectedIds.length === 0) {
-    console.warn('[changeElementComputedData] No elements selected')
-    return
-  }
-
   transactionApis.startTransaction()
-  elementApis.changeComputedData(selectedIds, key, data)
+  elementApis.changeComputedData(selectionApis.getSelectedIds(), key, data)
   transactionApis.endTransaction()
 }
