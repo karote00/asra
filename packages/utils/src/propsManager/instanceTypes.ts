d0a9ad2b57188af0153432b97c02e3602db11b75
Asa Tsai <karoteglathe@gmail.com>
2026-02-15 19:07:26 +0800

    fix: add index signatures to props-manager types
    - Add [key: string]: unknown to PositionAttrs, DimensionAttrs, and FillAttrs
    - Fixes build error: Type does not satisfy constraint PropertyComponentInstanceDataTypes
    - These types use union types with & Record<string, unknown>, so they need index signatures
    - Build now passes successfully

--- packages/utils/src/propsManager/instanceTypes.ts
+++ packages/utils/src/propsManager/instanceTypes.ts
@@ -12,20 +12,24 @@ export interface BasePropertyAttrs {
 export interface PositionAttrs extends BasePropertyAttrs, PositionData {
   xUnit: Unit
   yUnit: Unit
+  [key: string]: unknown
 }
 
 export interface DimensionAttrs extends BasePropertyAttrs, DimensionData {
   widthUnit: Unit
   heightUnit: Unit
+  [key: string]: unknown
 }
 
 export interface FillAttrs {
   color: number[]
   opacity: number
+  [key: string]: unknown
 }
 
-export interface IProperty<T extends BasePropertyAttrs = BasePropertyAttrs>
-  extends ISetter<T> {
+export interface IProperty<
+  T extends BasePropertyAttrs = BasePropertyAttrs
+> extends ISetter<T> {
   load(data: Partial<PropertyComponentRawData>): void
   save(): PropertyComponentRawData
   getValue(): Record<string, DataTypes>
@@ -34,8 +38,7 @@ export interface IProperty<T extends BasePropertyAttrs = BasePropertyAttrs>
 export interface Position extends IProperty {}
 export interface Dimension extends IProperty {}
 
-export interface PropertyComponentInstanceTypes
-  extends IProperty<PropertyComponentInstanceDataTypes> {}
+export interface PropertyComponentInstanceTypes extends IProperty<PropertyComponentInstanceDataTypes> {}
 export type PropertyComponentInstanceDataTypes = (
   | PositionAttrs
   | DimensionAttrs
